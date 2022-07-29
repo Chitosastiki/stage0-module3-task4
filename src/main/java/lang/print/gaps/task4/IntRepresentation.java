6da2b54903991436372b9a842e5e3a49e88b047f
Chitosastiki <110228508+Chitosastiki@users.noreply.github.com>
2022-07-30 01:15:35 +0400

    Update IntRepresentation.java

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,15 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        
+        char ch1 = 'c';
+            char ch2 = 'h';
+            char ch3 = 'a';
+            char ch4 = 'r';
+            int charc = ch1;
+            int charh = ch2;
+            int chara = ch3;
+            int charr = ch4;
+            System.out.println(ch1 , ch2 , ch3 , ch4 )
     }
 }
