c42b42accd64cc8f12f9c6bc77372e328231fe5f
Chitosastiki <110228508+Chitosastiki@users.noreply.github.com>
2022-07-30 01:41:43 +0400

    Update FormulaCreator.java

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,8 +3,8 @@ package lang.print.gaps.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
         
-        long a;
-        long b;
+        int a;
+        int b;
         float c = (9 * a^2 - 5*b + 2 + a +7)*((a + b -4 * a * b) / 2);
    
     }
